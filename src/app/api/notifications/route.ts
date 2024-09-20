327d1986d947c721218649e4710211e0ebe59579
jvhTech8021 <jvhtechinnovation@gmail.com>
2024-09-20 13:47:40 -0500

    chaning notifs endpoint to GET

--- src/app/api/notifications/route.ts
+++ src/app/api/notifications/route.ts
@@ -1,18 +1,6 @@
 import { NextRequest } from "next/server";
 
-// export default async function handler(req, res) {
-//     if (req.method === 'POST') {
-//       const { message, eventData } = req.body;
-//       console.log('Data from Lambda:', message, eventData);
-  
-//       // Process the data and potentially update your UI
-//       res.status(200).json({ success: true });
-//     } else {
-//       res.status(405).json({ message: 'Method Not Allowed' });
-//     }
-//   }
-
-  export async function POST(request: NextRequest) {
+  export async function GET(request: NextRequest) {
     const params = request;
-    console.log
+    console.log('test this', params)
   }
