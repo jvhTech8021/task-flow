f6ab460745cd8590878757b57a2e736b95155cf3
jvhTech8021 <jvhtechinnovation@gmail.com>
2024-09-20 13:06:09 -0500

    notifs route

---
+++ src/app/api/notifications/route.js
@@ -0,0 +1,11 @@
+export default async function handler(req, res) {
+    if (req.method === 'POST') {
+      const { message, eventData } = req.body;
+      console.log('Data from Lambda:', message, eventData);
+  
+      // Process the data and potentially update your UI
+      res.status(200).json({ success: true });
+    } else {
+      res.status(405).json({ message: 'Method Not Allowed' });
+    }
+  }
