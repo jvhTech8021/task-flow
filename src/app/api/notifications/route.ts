dcc4419a611f489f3c7b0c2970e14bbc6efba718
jvhTech8021 <jvhtechinnovation@gmail.com>
2024-09-20 17:07:13 -0500

    returning test

--- src/app/api/notifications/route.ts
+++ src/app/api/notifications/route.ts
@@ -3,4 +3,5 @@ import { NextRequest } from "next/server";
   export async function GET(request: NextRequest) {
     const params = request;
     console.log('test this', params)
+    return params
   }
