55b5fe19098dc1e1a9f60362f0aa3b6a1c2d2bbb
jvhTech8021 <jvhtechinnovation@gmail.com>
2024-09-26 22:03:51 -0500

    cleaning build error

--- src/app/internal/page.tsx
+++ src/app/internal/page.tsx
@@ -1,11 +1,6 @@
 import TasksTable from "@/app/internal/components/tasksTable"
-import { copilotApi } from "copilot-node-sdk";
 
 export default async function TaskPage() {
-
-  copilotApi.
-
-
   return (
     <>
       <TasksTable></TasksTable>
