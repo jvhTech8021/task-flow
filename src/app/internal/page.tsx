4b03079aa767bb7a21a77be4126dbd4127a64a36
jvhTech8021 <jvhtechinnovation@gmail.com>
2024-10-08 21:01:36 -0500

    improving speed

--- src/app/internal/page.tsx
+++ src/app/internal/page.tsx
@@ -2,7 +2,7 @@ import TasksFetcher from '@/app/internal/components/notificationFetch';
 
 export default function TaskPage() {
 
-  console.log(process.env.COPILOT_API_KEY)
+  console.log("THIS HERE", process.env.COPILOT_API_KEY)
   return (
     <div className="m-4">
       <TasksFetcher />
