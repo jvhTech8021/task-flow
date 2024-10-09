b3e88566c93de4ada2c2bc18436d1255eadea7da
jvhTech8021 <jvhtechinnovation@gmail.com>
2024-10-08 21:11:23 -0500

    improving speed

--- src/app/internal/page.tsx
+++ src/app/internal/page.tsx
@@ -5,8 +5,10 @@ export default function TaskPage() {
   console.log("THIS HERE", process.env.COPILOT_API_KEY)
   return (
     <div className="m-4">
-      {/* <TasksFetcher /> */}
-      What is this
+      {process.env.COPILOT_API_KEY}
+
+      <TasksFetcher />
+      {/* What is this */}
     </div>
   );
 }
